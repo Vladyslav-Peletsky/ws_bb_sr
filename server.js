4c49827de75cced74e03d391c345e432aa359a2a
vladyslav.peletsky <vladyslav.peletsky@saleit.com.ua>
2021-12-26 03:50:33 +0200

    fix

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ wsServer.on('connection', onConnect);
 
 function onConnect(wsClient) {
     console.log('Новый пользователь');
-    wsClient.send('Привет');
 
     wsClient.on('close', function() {
         console.log('Пользователь отключился');
