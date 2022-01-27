2db8d8e83367619885fe8976838e92c9a5623d6d
vladyslav.peletsky <vladyslav.peletsky@saleit.com.ua>
2022-01-27 02:15:31 +0200

    fix

--- database.js
+++ database.js
@@ -89,6 +89,7 @@ function sceneStatuses(uuid) {
         console.log('sceneStatuses _ uuid :' + uuid);
         let sceneStatuses = {"type":"sceneStatuses"};
         sceneStatuses.data = res.rows;
+        console.log('sceneStatuses - ' + JSON.stringify(sceneStatuses).replace('sceneid', 'sceneID'));
         resolve(JSON.stringify(sceneStatuses).replace('sceneid', 'sceneID'));
     });
     
