2014d39d7f43170f19a7f5872b6af7457330d9d9
vladyslav.peletsky <vladyslav.peletsky@saleit.com.ua>
2022-01-29 19:35:24 +0200

    fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //import * as site from './site.js';
 import {createTables,dropTables} from './createTablesDB.js';
-//import {onConnect} from './ws.js';
+import {onConnect} from './ws.js';
 
 dropTables();
 setTimeout(createTables, 1000); 
