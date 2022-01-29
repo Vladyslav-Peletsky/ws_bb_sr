f7aa0c61f2abf08d0394c6dae6cac705bc641f6c
vladyslav.peletsky <vladyslav.peletsky@saleit.com.ua>
2022-01-29 19:49:38 +0200

    fix

--- site.js
+++ site.js
@@ -2,6 +2,7 @@ import * as http from 'http';
 import * as fs from 'fs';
 
 const PORT=process.env.PORT || 8080; 
+console.log('site must on port: '+PORT)
 
     fs.readFile('./www/index.html', function (err, html) {
 
@@ -11,5 +12,6 @@ const PORT=process.env.PORT || 8080;
             response.writeHeader(200, {"Content-Type": "text/html"});  
             response.write(html);  
             response.end();  
+            console.log('start site: '+ PORT);
         }).listen(PORT);
     });
