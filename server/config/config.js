e38e794e7ae84f86521937b0c1fad2c004c19d76
vladyslav.peletsky <vladyslav.peletsky@saleit.com.ua>
2022-04-06 01:21:30 +0300

    fix

--- server/config/config.js
+++ server/config/config.js
@@ -2,6 +2,7 @@ let config = {};
 
 //GLOBAL
 config.domain = 'https://ws-bb-sr.herokuapp.com/';
+// (// (Vercel Supabase)) config.dbPostgresURI = 'postgresql://postgres:188123ZxcvbZ@db.uzljczoqzroarxbqyrjy.supabase.co:5432/postgres';
 config.dbPostgresURI = 'postgres://txstbteobiwucu:4f3b9f560b347b9a39b035edace61fddeb5d8f93d6635779f69b215d746e43b9@ec2-54-247-137-184.eu-west-1.compute.amazonaws.com:5432/d6sct12bbs92ns';
 config.db_name = 'd6sct12bbs92ns';
 
