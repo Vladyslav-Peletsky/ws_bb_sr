8f9e032e39cdf34f74a05edac039f701ab2a93e3
vladyslav.peletsky <vladyslav.peletsky@saleit.com.ua>
2022-04-17 15:41:13 +0300

    fix

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,9 @@
 let config = {};
 
 //GLOBAL
-config.domain = 'https://ws-bb-sr.herokuapp.com/';
-// (// (Vercel Supabase)) config.dbPostgresURI = 'postgresql://postgres:188123ZxcvbZ@db.uzljczoqzroarxbqyrjy.supabase.co:5432/postgres';
-config.dbPostgresURI = 'postgres://txstbteobiwucu:4f3b9f560b347b9a39b035edace61fddeb5d8f93d6635779f69b215d746e43b9@ec2-54-247-137-184.eu-west-1.compute.amazonaws.com:5432/d6sct12bbs92ns';
-config.db_name = 'd6sct12bbs92ns';
-
-config.offline_api_token = '8CEB1B0C-1FEB-48EA-8F96-BB4DDBBB06D9';
+config.domain = 'https://ws-bb-sr-demo.herokuapp.com/';
+config.dbPostgresURI = process.env.DATABASE_URL;
+config.offline_api_token = process.env.OFFLINE_API_TOKEN;
 
 //PATH
 config.public_path = process.cwd() + '/public/';
